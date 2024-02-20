a9d15aafcd4511e81109f1c0fd410a5e33230cdf
Ajoy-paul11 <ajoypaul8875@gmail.com>
2024-02-20 22:45:41 +0530

    Fetch the news to the page

--- News_Application/app.js
+++ News_Application/app.js
@@ -10,5 +10,35 @@ async function getnews(query) {
     const response = await fetch(`${url}${query}&apiKey=${API_KEY}`)
     const data = await response.json()
     console.log(data);
-    displayNews(data)
-} 
+    displayNews(data.articles)
+}
+
+function displayNews(data) {
+    const cardsBox = document.getElementById('cards-box')
+    const cardTemplate = document.getElementById('card-template')
+
+    cardsBox.innerHTML = ""
+
+    data.forEach(article => {
+        if (!article.urlToImage) return
+
+        const newCard = cardTemplate.content.cloneNode(true)
+        dataFill(newCard, article)
+        cardsBox.appendChild(newCard)
+    });
+}
+
+function dataFill(newCard, article) {
+    const cardImg = newCard.querySelector('#news-img')
+    const cardTitle = newCard.querySelector('#title')
+    const cardPublisher = newCard.querySelector('.publisher')
+    const cardArticle = newCard.querySelector('#article')
+
+    cardImg.src = article.urlToImage
+    cardTitle.innerText = article.title
+    cardArticle.innerText = article.description
+
+    const date = new Date(article.publishedAt).toLocaleString("en-US", { timeZone: "Asia/Jakarta" })
+
+    cardPublisher.innerText = `${article.source.name} • ${date}`
+}
